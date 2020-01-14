73b4ac1e73259497a3c891710ad52337587e0054
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-01-14 16:04:51 -0300

    Desenvolvendo o backend da aplicação

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,31 @@
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../util/ParseStringAsArray')
+
+
+module.exports = {
+  async index(req, res) {
+    //buscar devs num raio de 10km
+    //filtrar por tecnologias
+    const {latitude, longitude, techs} = req.query;
+    
+    const techsArray = parseStringAsArray(techs);
+
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray,
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000,
+        },
+      },
+    })
+
+    return res.json({devs})
+
+  }
+}
